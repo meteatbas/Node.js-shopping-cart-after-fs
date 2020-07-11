41849cbbd3adbd39ae7299cd39d0545794a07a20
meteatbas <65925112+meteatbas@users.noreply.github.com>
2020-07-11 15:12:15 +0300

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const MONGODB_URI =
-  'mongodb+srv://techmete:i7xrnMcCIBO9surM@node-js-shopping-site.fplbl.mongodb.net/shop?retryWrites=true&w=majority';
+  '';
 
 const app = express();
 const store = new MongoDBStore({
